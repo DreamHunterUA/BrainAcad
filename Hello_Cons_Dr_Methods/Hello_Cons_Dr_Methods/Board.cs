6acf4d7fa7d300a8dca448159ee697d94788274f
Alexandr Baryshenko <a.baryshenko@p-sec.eu>
2015-10-29 10:19:49 +0200

    add lab draw box with message

---
+++ Hello_Cons_Dr_Methods/Hello_Cons_Dr_Methods/Board.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hello_Cons_Dr_Methods
+{
+    class Board
+    {
+        public int X { get; private set; }
+        public int Y { get; private set; }
+        public int Width { get; private set; }
+        public int Height { get; private set; }
+        public char Symbol { get; private set; }
+        public string Message { get; private set; }
+
+
+        public Board(int x, int y, int width, int heght, char symbol = '*', string message = "Nan")
+        {
+            X = x;
+            Y = y;
+            Width = width;
+            Height = heght;
+            Symbol = symbol;
+            Message = message;
+
+
+        }
+
+        private bool CheckInputData()
+        {
+            if (X == 0)
+                return false;
+            if (Y == 0)
+                return false;
+            if (Message.Length > (Width - X - 2))
+                return false;
+            if (Y > Height)
+                return false;
+            if (X > Width)
+                return false;
+            return true;
+        }
+
+        public void Draw()
+        {
+            if (CheckInputData())
+            {
+                //draw board
+                Console.Clear();
+                //if input data valid draw board
+                for (int i = 0; i <= Height; i++)
+                {
+                    for (int j = 0; j <= Width; j++)
+                    {
+                        if ((i == 0)||(j==Height)||(i==Width)||(j==0))
+                        {
+                            Console.SetCursorPosition(i, j);
+                            Console.Write(Symbol);
+                        }
+                        if((i==X) && (j ==Y))
+                        {
+                            Console.SetCursorPosition(i, j);
+                            Console.Write(Message);
+                        }
+
+                    }
+                }
+            }
+            else
+            //else write invalid message            
+            {
+                Console.WriteLine("Warning input data invalid!!!!");
+            }
+        }
+
+    }
+}
---
+++ Hello_Cons_Dr_Methods/Hello_Cons_Dr_Methods/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hello_Cons_Dr_Methods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Enter the width of the board");
+
+            int width;
+            while (!Int32.TryParse(Console.ReadLine(), out width))
+            {
+                Console.WriteLine("Please enter a valid width value");  
+            }
+                    
+            Console.WriteLine("Enter the height of the board");
+            int height;
+            while (!Int32.TryParse(Console.ReadLine(), out height))
+            {
+                Console.WriteLine("Please enter a valid height value");
+            }
+               
+            Console.WriteLine("Enter the symbol of the board");
+            var symbol = Console.ReadLine();
+            if ((Convert.ToChar(symbol)!='*')||(Convert.ToChar(symbol) != '+')|| (Convert.ToChar(symbol) != '.'))
+            {
+                symbol = "*";
+            }
+            Console.WriteLine("Enter left position of the message");
+            int x;
+            while (!Int32.TryParse(Console.ReadLine(), out x))
+            {
+                Console.WriteLine("Please enter a valid left position value");
+            }
+            
+            Console.WriteLine("Enter top position of the message");
+            int y;
+            while (!Int32.TryParse(Console.ReadLine(), out y))
+            {
+                Console.WriteLine("Please enter a valid top position value");
+            }
+            Console.WriteLine("Enter message your want to print");
+            var message = Console.ReadLine();
+
+            var board = new Board(x, y, width, height, Convert.ToChar(symbol), message);
+            //var board = new Board(1, 3, 10, 10);
+            board.Draw();
+            Console.WriteLine("\n");
+            Console.WriteLine("Press any key...");
+
+            Console.ReadLine();
+
+
+        }
+    }
+}
